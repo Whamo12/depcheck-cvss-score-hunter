27229bb60d02bf6dc0ba700b45b9b7eaa8ae2734
Alejandro Saenz <alejandrosaenz117@gmail.com>
2017-12-13 13:22:44 -0500

    New jar

--- src/CvssScoreHunter.java
+++ src/CvssScoreHunter.java
@@ -80,6 +80,7 @@ public class CvssScoreHunter {
 								.userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
 								.referrer("http://www.google.com")
 								.validateTLSCertificates(false)
+								.followRedirects(true)
 								.get();
 						
 						getCvssScores(cveDoc);
@@ -90,6 +91,7 @@ public class CvssScoreHunter {
 								.userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
 								.referrer("http://www.google.com")
 								.validateTLSCertificates(false)
+								.followRedirects(true)
 								.get();
 						
 						getCvssScores(cveDoc);
